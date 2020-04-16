0858a4a38023b2171ae4da6c550bfabff343c661
Manish Kumar Choudhary <manishkumar.choudhary@hitachivantara.com>
2020-04-16 21:43:07 +0530

    Trying to fix home page naviation

---
+++ precache-manifest.46f77dbc91e74eca05ff1cd7d0b933ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3973c80e1753a2080e460913c9630f1b",
+    "url": "/codingpro/index.html"
+  },
+  {
+    "revision": "aad102c52b524e2fba74",
+    "url": "/codingpro/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1a4253fb8247b28e2ff3",
+    "url": "/codingpro/static/js/2.bd178c72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codingpro/static/js/2.bd178c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad102c52b524e2fba74",
+    "url": "/codingpro/static/js/main.aca94c77.chunk.js"
+  },
+  {
+    "revision": "f913bd36e3b3db166a39",
+    "url": "/codingpro/static/js/runtime-main.a73d34fe.js"
+  }
+]);
