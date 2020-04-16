c74e079a80fa11670965a2b7070a17b801b5c4f6
Manish Kumar Choudhary <manishkumar.choudhary@hitachivantara.com>
2020-04-16 22:51:28 +0530

    Auto Deployment of APP 1

--- AppDeployent.cmd
+++ AppDeployent.cmd
@@ -1,15 +1,15 @@
 @ECHO OFF
 
-CALL SET SOURCEDIR=./WebApp
-CALL SET DESTINATIONDIR=.
+REM CALL SET SOURCEDIR=./WebApp
+REM CALL SET DESTINATIONDIR=.
 REM CALL git clone -b gh-pages --single-branch https://github.com/manishki/codingpro-app.git %SOURCEDIR%
 
-CALL robocopy %SOURCEDIR% %DESTINATIONDIR%  /XD .git
+REM CALL robocopy %SOURCEDIR% %DESTINATIONDIR% /E /XD .git
 REM CALL timeout 30
-
+REM CALL rd /s /q %SOURCEDIR%
 
 CALL git status
 CALL git add .
 CALL git status
-CALL git commit -m "Auto Deployment of APP"
+CALL git commit -m "Auto Deployment of APP 1"
 CALL git push origin master
