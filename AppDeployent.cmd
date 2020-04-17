461b1715f2c4e71299dfd7ff04e44a69eccf4266
Manish Kumar Choudhary <manishkumar.choudhary@hitachivantara.com>
2020-04-17 22:32:10 +0530

    Adding contact us and icons

--- AppDeployent.cmd
+++ AppDeployent.cmd
@@ -3,12 +3,11 @@
 CALL SET SOURCEDIR=./WebApp
 CALL SET DESTINATIONDIR=.
 
-del WebApp\*.* /s /q
+REM del WebApp\*.* /s /q
 CALL git clone -b gh-pages --single-branch https://github.com/manishki/codingpro-app.git %SOURCEDIR%
 
 CALL robocopy %SOURCEDIR% %DESTINATIONDIR% /E /XD .git
 CALL timeout 10
-REM CALL rd /s /q %SOURCEDIR%
 
 CALL git status
 CALL git add .
---
+++ precache-manifest.29f65422b7b54da14167fdce753ef60f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd22db7f8f878c51d1235a13278af94f",
+    "url": "/codingpro/index.html"
+  },
+  {
+    "revision": "0d2603852b1b9a20a593",
+    "url": "/codingpro/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c6f73e54c21d14d85ba6",
+    "url": "/codingpro/static/js/2.ec3078ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codingpro/static/js/2.ec3078ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2603852b1b9a20a593",
+    "url": "/codingpro/static/js/main.5d16adbc.chunk.js"
+  },
+  {
+    "revision": "f913bd36e3b3db166a39",
+    "url": "/codingpro/static/js/runtime-main.a73d34fe.js"
+  }
+]);
